eba6f9803247d208e6d647bf1cd38b82d8f4c81c
LucianoPiancatelli <42613779+LucianoPiancatelli@users.noreply.github.com>
2022-11-15 17:01:41 -0300

    Initial commit

---
+++ test.js
@@ -0,0 +1,23 @@
+const Contenedor = require('./Contenedor.js')
+const express = require('express')
+const app = express()
+const PORT = 8080
+const contenedor = new Contenedor('./productos.txt')
+const log = (p) => console.log(p)
+
+
+
+app.get('/', async (req, res)=>{
+    res.send('<h1>Recorda redireccionar a las rutas /productos o /productosRandom</h1>')
+})
+app.get('/productos', async (req, res)=>{
+    res.send(await contenedor.getAll())
+})
+app.get('/productosRandom', async (req, res)=>{
+    res.send(await contenedor.getRandom())
+});
+
+
+const server = app.listen(PORT, ()=>{
+    console.log(`escuchando ${server.address().port}`)
+})
